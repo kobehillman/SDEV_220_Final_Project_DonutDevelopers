c4f91a4de00549db7264e136b237a2102a722575
Kobe Hillman <131305116+kobehillman@users.noreply.github.com>
2024-03-03 01:44:36 -0500

    Add files via upload
    - Added new buttons
    - Changed theme
    - Changed About Us page
    - Added picture on About Us page
    - Added logo to main window 
    - Implemented customtkinter
    - Implemented Pillow
    
    - Added requirements.txt file

--- FinalProject_DonutDevelopers.py
+++ FinalProject_DonutDevelopers.py
@@ -2,67 +2,198 @@
 # Date Written:
 # FinalProject_DonutDevelopers
 
-import tkinter
 # Import the required Libraries
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import PIL.ImageTk
+from PIL import Image
+import customtkinter
 
-welcomeWindow = Tk()
 
-def Menu():
-    Menu = tkinter.Toplevel()
-    Menu.geometry("750x250")
-    Menu.title = ('Wecome to Mary Lou Donuts!')
-    # Create Button(s)
-    Label(Menu, text="Here's our collection of Donuts!").pack(pady=20)
-    Button(Menu, text=("Donut 1"), width=20).pack(pady=15)
-    Button(Menu, text=("Donut 2"), width=20).pack(pady=15)
-    Button(Menu, text=("Donut 3"), width=20).pack(pady=15)
-    Button(Menu, text=("Donut 4"), width=20).pack(pady=15)
-    Button(Menu, text=("Donut 5"), width=20).pack(pady=15)
+class DonutApp(tk.Tk):
 
-def aboutUs():
-    aboutUs = tkinter.Toplevel()
-    aboutUs.geometry("750x250")
-    aboutUs.title = ('Wecome to Mary Lou Donuts!')
+    def __init__(self):
+        super().__init__()
+
+        def exit_app():
+            tk.Tk.destroy(self)
+
+        class MenuPage(DonutApp):
+
+            @staticmethod
+            def load_menu():
+                menu_window = tk.Toplevel()
+                menu_window.iconbitmap("donut.ico")
+                menu_window.geometry("750x400")
+                menu_window.title = 'Welcome to Mary Lou Donuts!'
+                # Create Button(s)
+                menu_label = tk.Label(menu_window, text="Here's our collection of Donuts!")
+                menu_label.pack(pady=20)
+
+                menu_button_one = tk.Button(menu_window, text="Glazed Ring", width=20)
+                menu_button_one.pack(pady=15)
+
+                menu_button_two = tk.Button(menu_window, text="Sprinkled Ring", width=20)
+                menu_button_two.pack(pady=15)
+
+                menu_button_three = tk.Button(menu_window, text="Chocolate Cake", width=20)
+                menu_button_three.pack(pady=15)
+
+                menu_button_four = tk.Button(menu_window, text="Tiger Tail", width=20)
+                menu_button_four.pack(pady=15)
+
+                menu_button_five = tk.Button(menu_window, text="Vanilla Cake", width=20)
+                menu_button_five.pack(pady=15)
+
+        class AboutUsPage(DonutApp):
+
+            @staticmethod
+            def load_about_us():
+                about_us_window = tk.Toplevel(bg='#11c4ff')
+                about_us_window.resizable(False, False)
+                about_us_window.iconbitmap("donut.ico")
+                about_us_window.geometry("950x600")
+                about_us_window.title = 'Welcome to Mary Lou Donuts!'
+                # Create Label & Button(s)
+                about_us_text = tk.Label(about_us_window, fg='#FFFFFF', text="Welcome to Mary Lou Donuts, where "
+                                                                             "tradition meets taste"
+                                                                             " in every bite!\n\n"
+                                                                             "Since 1961, our cozy shop in Lafayette, "
+                                                                             "Indiana,"
+                                                                             "\n has been"
+                                                                             "a community staple, serving up "
+                                                                             "delectable,"
+                                                                             "freshly-made\n"
+                                                                             "donuts that evoke the sweet nostalgia "
+                                                                             "of home.\n\n"
+                                                                             "Here, every donut is a piece of our "
+                                                                             "story, crafted with"
+                                                                             "love and the finest ingredients.\nAs "
+                                                                             "you explore our"
+                                                                             "app,"
+                                                                             "discover our diverse menu, learn about "
+                                                                             "our specials,"
+                                                                             "and order online.\n"
+                                                                             "\nJoin us in celebrating the joy of "
+                                                                             "simple, delicious"
+                                                                             "donuts and more made just for you.",
+                                         font='Helvetica', bg='#ed7dd4')
+                about_us_text.pack(pady=20)
+
+                # Loads background image and resizes it
+                image_path = PIL.Image.open('about_us_image.png')
+                image = PIL.ImageTk.PhotoImage(image_path.resize((300, 300)))
+
+                # Creates a 'background' label and attaches the image to the label
+                about_image_label = tk.Label(about_us_window, bg='#ed7dd4', bd=1, image=image)
+                about_image_label.image = image
+                about_image_label.place(x=330, y=250)
+                # Loads background image and resizes it
+
+        class PlaceOrderPage(DonutApp):
+
+            @staticmethod
+            def place_order():
+                place_order_window = tk.Toplevel()
+                place_order_window.iconbitmap("donut.ico")
+                place_order_window.geometry("750x500")
+                place_order_window.title = 'Text'
+                # Create Label & Button(s)
+                place_order_text = tk.Label(place_order_window, text="Place your order here!")
+                place_order_text.pack(pady=20)
+
+                order_option_one = tk.Button(place_order_window, text="Glazed Ring", width=20)
+                order_option_one.pack(pady=10)
+                sb_one = tk.Spinbox(place_order_window, from_=0, to=20)
+                sb_one.pack()
+
+                order_option_two = tk.Button(place_order_window, text="Sprinkled Ring", width=20)
+                order_option_two.pack(pady=10)
+                sb_two = tk.Spinbox(place_order_window, from_=0, to=20)
+                sb_two.pack()
+
+                order_option_three = tk.Button(place_order_window, text="Chocolate Cake", width=20)
+                order_option_three.pack(pady=10)
+
+                sb_three = tk.Spinbox(place_order_window, from_=0, to=20)
+                sb_three.pack()
+
+                finish_order_button = tk.Button(place_order_window, text="Finish Order", width=20, command=exit_app)
+                finish_order_button.pack(pady=1)
+
+        # prices = {"Glazed Ring": 1.25, "Sprinkled Ring": 1.25, "Chocolate Cake": 1.25}
+
+        # Changes size of the main window, adds app icon to window, window resizable T/F
+        # Sets the title of the main window.
+        # Sets the background color to white.
+
+        main_logo_path = PIL.Image.open('mary_lou_donuts.png')
+        main_logo = PIL.ImageTk.PhotoImage(main_logo_path.resize((200, 200)))
+
+        main_logo_label = tk.Label(self, bg='#FFFFFF', image=main_logo)
+        main_logo_label.image = main_logo
+        main_logo_label.place(x=500, y=35)
+
+        self.title("Mary Lou Donuts")
+        self.geometry("750x250")
+        self.iconbitmap("donut.ico")
+        self.resizable(False, False)
+        self.configure(bg='#FFFFFF')
+
+        # Label that displays text on main window
+        welcome_label = tk.Label(self, bg='#11c4ff', text="Welcome to Mary Lou Donuts!", fg='#FFFFFF', font='Helvetica')
+        welcome_label.pack(pady=10)
+
+        # Defines Place Order button image
+        # Adds a "Place Order" button that takes the user to our order window allowing them to place an order.
+        place_order_image = customtkinter.CTkImage(Image.open('place_order.png'), size=(30, 30))
+
+        place_order_button = customtkinter.CTkButton(self, image=place_order_image, text="Place Order",
+                                                     command=PlaceOrderPage.place_order, compound='left',
+                                                     fg_color='#f06eaa', hover_color='#11c4ff')
+        place_order_button.pack(pady=5)
+
+        # Defines About Us button image
+        # Adds an "About Us" button that takes the user to our About Us page.
+        about_us_button_image = customtkinter.CTkImage(Image.open('about_us_button_image.png'), size=(30, 30))
+
+        about_button = customtkinter.CTkButton(self, image=about_us_button_image, text="About Us",
+                                               command=AboutUsPage.load_about_us, compound='right', fg_color='#f06eaa',
+                                               hover_color='#11c4ff')
+        about_button.pack(pady=5)
+
+        # Defines Menu button image
+        # Adds a "Menu" button that loads our menu in a different window.
+        menu_button_image = customtkinter.CTkImage(Image.open('menu_button_image.png'), size=(30, 30))
+
+        menu_button = customtkinter.CTkButton(self, image=menu_button_image, text="Menu", command=MenuPage.load_menu,
+                                              compound='left', fg_color='#f06eaa', hover_color='#11c4ff')
+        menu_button.pack(pady=5)
+
+        # Adds an "Exit" button that closes the application.
+        exit_button = customtkinter.CTkButton(self, fg_color="#E50000", hover_color='#FF0000', text="Exit",
+                                              command=self.destroy)
+        exit_button.pack(pady=5)
+
+
+def main():
+    DonutApp().mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
+    # DO NOT DELETE - BELOW IS THE BASE SETUP FOR ANY WINDOW
+    # Replace all '?'s with the variable you want set up for the window
+    # Make sure that the '!' matches the command listed in the button that goes to this window
+    # def !():
+    #   ? = tk.Toplevel()
+    #   ?.geometry("750x250")
+    #   ?.title = ('Text')
     # Create Label & Button(s)
-    Label(aboutUs, text="This is out History! \n This is what we do! \n This is some other third thing!").pack(pady=20)
-
-def placeOrder():
-    placeOrder = tkinter.Toplevel()
-    placeOrder.geometry("750x300")
-    placeOrder.title = ('Text')
-    # Create Label & Button(s)
-    Label(placeOrder, text="Place your order here!"
-                          "\nWe need to figure out a way to increment a variable everytime someone presses a buton"
-                          "\nThat way we can tell how many of something someone orders, instead only being able to order 1"
-                          "\nThis could be done with A LOT of windows, but it would be messy and hard to read"
-                          "\nBTW the 'Finish Order' Button just closes everything until we have confirmation window").pack(pady=20)
-    Button(placeOrder, text=("Placeholder Item1"), width=20).pack(pady=10)
-    Button(placeOrder, text=("Placeholder Item2"), width=20).pack(pady=10)
-    Button(placeOrder, text=("Placeholder Item3"), width=20).pack(pady=10)
-    Button(placeOrder, text=("Finish Order"), width=20, command = welcomeWindow.destroy).pack(pady=1)
-
-welcomeWindow.geometry("750x250")
-# welcomeWindow.title = ('Welcome to Mary Lou Donuts!')
-# Create Button(s)
-Label(welcomeWindow, text="Welcome to Mary Lou Donuts! Please choose from the following options.").pack(pady=20)
-welcomeWindow.geometry("750x250")
-
-Button(welcomeWindow, text=("About Us"), width=20,command = aboutUs).pack(pady=0)
-Button(welcomeWindow, text=("Place Order"), width=20, command = placeOrder).pack(pady=0)
-Button(welcomeWindow, text=("Menu"), width=20, command=Menu).pack(pady=0)
-Button(welcomeWindow, text="Leave", width=20, command = welcomeWindow.destroy).pack(pady=0)
-
-welcomeWindow.mainloop()
-
-# DO NOT DELETE - BELOW IS THE BASE SETUP FOR ANY WINDOW
-# Replace all '?'s with the variable you want set up for the window
-# Make sure that the '!' matches the command listed in the button that goes to this window
-#def !():
-#   ? = tkinter.Toplevel()
-#   ?.geometry("750x250")
-#   ?.title = ('Text')
-    # Create Label & Button(s)
-#   Label(?, text="Text").pack(pady=20)
-#   Button(?, text=("Text"), width=20).pack(pady=15)
+    #   ? = tk.Label(?, text="Text").pack(pady=20)
+    #   ? = tk.Button(?, text=("Text"), width=20).pack(pady=15)
+    #
+    # FOR CUSTOM TKINTER BUTTONS ETC!!!
+    # ? = customtkinter.CTkButton(?, text='Text')
+    # ?.pack() or .place()
+    #
